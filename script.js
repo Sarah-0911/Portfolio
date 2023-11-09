a8173c61bb14c8e282e97fcba5f91ee8750f03e9
Sarah-0911 <sarah.tabet69@gmail.com>
2023-11-09 01:36:59 +0100

    add hello + description in progress

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Button Know More
+
 const knowButton = document.getElementById('know-btn');
 const linkText = knowButton.querySelector("a");
 
@@ -11,3 +13,14 @@ const originColor = () => {
 
 knowButton.addEventListener('mousedown', turnToGreen);
 knowButton.addEventListener('mouseup', originColor);
+
+// Zoom effect on Card
+
+// const cards = document.querySelectorAll(".card");
+
+// cards.forEach(card => {
+//   card.addEventListener('mouseover', () =>
+//   card.style.backgroundSize = '120%');
+//   card.addEventListener('mouseout', () =>
+//   card.style.backgroundSize = '100%');
+// })
